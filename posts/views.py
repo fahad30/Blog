a546151f33bacaa81bae61da7c71338641617fd9
Fhd30 <fahad.a.suram@gmail.com>
2017-07-24 15:30:45 +0300

    my first commit

---
+++ posts/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+def post_create(request):
+    object_list = Post.objects.all()
+    context = {
+    "object_list": object_list,
+    "title": "Create",
+    "user": request.user
+    }
+    return render(request, 'post_create.html', context)
+
+from django.shortcuts import get_object_or_404
+
+def post_detail(request):
+    instance = get_object_or_404(Post, id=1)
+    context = {
+    "title": "Detail",
+    "instance": instance
+    }
+    return render(request, 'post_detail.html', context)
+
+def post_list(request):
+    object_list = Post.objects.all()
+    context = {
+    "object_list": object_list,
+    "title": "List",
+    "user": request.user
+    }
+    return render(request, 'post_list.html', context)
+
+def post_update(request):
+    object_list = Post.objects.all()
+    context = {
+    "object_list": object_list,
+    "title": "Update",
+    "user": request.user
+    }
+    return render(request, 'post_update.html', context)
+
+def post_delete(request):
+    object_list = Post.objects.all()
+    context = {
+    "object_list": object_list,
+    "title": "delete",
+    "user": request.user
+    }
+    return render(request, 'post_delete.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
