f0f3aab0075534ef7f05cf3c569fe20e0b80858f
Fhd30 <fahad.a.suram@gmail.com>
2017-07-26 21:01:53 +0300

    image file

--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,10 @@ from .models import Post
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
         messages.success(request, "Done")
@@ -16,9 +17,8 @@ def post_create(request):
      }
     return render(request, 'post_create.html', context)
 
-
 def post_detail(request, post_id):
-    object_list = Post.objects.all()#.order_by("-timestamp", "-updated")
+    object_list = Post.objects.all()
     instance = get_object_or_404(Post, id=post_id)
     context = {
     "title": "Detail",
@@ -27,16 +27,28 @@ def post_detail(request, post_id):
     return render(request, 'post_detail.html', context)
 
 def post_list(request):
-    object_list = Post.objects.all()
+    object_list = Post.objects.all()#.order_by("-timestamp", "-updated")
+
+    paginator = Paginator(object_list, 5) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        objects = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        objects = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        objects = paginator.page(paginator.num_pages)
     context = {
     "title": "List",
-    "object_list": object_list
+    "object_list": objects
     }
     return render(request, 'post_list.html', context)
 
 def post_update(request, post_id):
     post_object = get_object_or_404(Post, id=post_id)
-    form = PostForm(request.POST or None, instance=post_object)
+    form = PostForm(request.POST or None, request.FILES or None, instance=post_object)
     if form.is_valid():
         form.save()
         messages.success(request, "Cool")
@@ -51,36 +63,3 @@ def post_delete(request, post_id):
     Post.objects.get(id=post_id).delete()
     messages.warning(request, "really")
     return redirect("posts:list")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
